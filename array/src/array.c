7c2cceb3ed6d4b1010d7aac183c228f48e21f3e2
jithin varghese <jithin6manjapra@gmail.com>
2021-07-23 11:22:33 +1200

    starting if arrays

---
+++ array/src/array.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : array.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a[50];
+	int n,i;
+	setbuf(stdout, NULL);
+	printf("Enter the n numbers");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+		{
+		scanf("%d",&a[i]);
+		}
+	for(i=0;i<n;i++)
+		{
+			printf("%d",a[i]);
+		}
+	return EXIT_SUCCESS;
+}
