a6b335ed5673fe82f49756e2d2210d777cb30050
jithin varghese <jithin6manjapra@gmail.com>
2021-07-23 13:52:33 +1200

    function with argument and with return value

---
+++ function_type3/src/function_type3.c
@@ -0,0 +1,34 @@
+/*
+ ============================================================================
+ Name        : function_type3.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int subtract(int,int);
+int main(void) {
+
+	int num1,num2;
+	int sub1;
+	setbuf(stdout, NULL);
+	printf("Enter the 2 numbers to subtract\n");
+	scanf("%d%d",&num1,&num2);
+
+	sub1=subtract(num1,num2);
+	printf("subtract= %d",sub1);
+
+	return EXIT_SUCCESS;
+}
+
+int subtract(int num1,int num2)
+{
+	int sub=0;
+	sub=num1-num2;
+	return sub;
+}
