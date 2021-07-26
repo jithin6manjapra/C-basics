35e3dd0906c46b5a02ff074c5baddf2a28d44d82
jithin varghese <jithin6manjapra@gmail.com>
2021-07-27 11:35:18 +1200

    final modifying

--- function4/src/function4.c
+++ function4/src/function4.c
@@ -11,7 +11,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int subtract();
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+
+	int sub1;
+	sub1=subtract();
+	printf("subtract= %d",sub1);
+
 	return EXIT_SUCCESS;
+}
+
+int subtract()
+{
+	int num1,num2;
+	int sub=0;
+		setbuf(stdout, NULL);
+		printf("Enter the 2 numbers to subtract\n");
+		scanf("%d%d",&num1,&num2);
+
+	sub=num1-num2;
+	return sub;
 }
